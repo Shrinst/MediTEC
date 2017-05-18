3a79a9a6cd171990907266f53340cc621d0c8122
varoqr08 <alvaroqr@hotmail.com>
2017-05-18 00:57:00 -0600

    Clase Correo
    Implementación de la clase cuya función es la de enviar el correo al
    usuario, falta aún darle interfaz

---
+++ Correo/src/Enviar_Correo.java
@@ -0,0 +1,40 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Enviar_Correo extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Enviar_Correo frame = new Enviar_Correo();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Enviar_Correo() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
---
+++ Correo/src/EnvioCorreo/Correo.java
@@ -0,0 +1,55 @@
+package EnvioCorreo;
+
+public class Correo {
+	private String usuarioCorreo;
+	private String contrasena;
+	private String rutaArchivo;
+	private String destino;
+	private String asunto;
+	private String nombreArchivo;
+	private String mensaje;
+	public String getUsuarioCorreo() {
+		return usuarioCorreo;
+	}
+	public void setUsuarioCorreo(String usuarioCorreo) {
+		this.usuarioCorreo = usuarioCorreo;
+	}
+	public String getContrasena() {
+		return contrasena;
+	}
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+	public String getRutaArchivo() {
+		return rutaArchivo;
+	}
+	public void setRutaArchivo(String rutaArchivo) {
+		this.rutaArchivo = rutaArchivo;
+	}
+	public String getDestino() {
+		return destino;
+	}
+	public void setDestino(String destino) {
+		this.destino = destino;
+	}
+	public String getNombreArchivo() {
+		return nombreArchivo;
+	}
+	public void setNombreArchivo(String nombreArchivo) {
+		this.nombreArchivo = nombreArchivo;
+	}
+	public String getMensaje() {
+		return mensaje;
+	}
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+	public String getAsunto() {
+		return asunto;
+	}
+	public void setAsunto(String asunto) {
+		this.asunto = asunto;
+	}
+	
+
+}
